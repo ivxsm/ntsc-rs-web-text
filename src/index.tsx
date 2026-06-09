bc243bf170ce060b8f5e6455d0b2c03efed62ea9
Khaled <121107036+ivxsm@users.noreply.github.com>
2026-06-10 01:04:39 +0300

    Add PostHog analytics integration and update .gitignore for environment variables,

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import {render} from 'preact';
+import posthog from 'posthog-js';
 
 import './css/fonts.css';
 import './css/global.scss';
@@ -10,6 +11,16 @@ import {OverlayProvider} from './components/Overlay/Overlay';
 import {ToastProvider} from './components/Toast/Toast';
 import {ContextMenuProvider} from './components/Widgets/Widgets';
 import PwaUpdatePrompt from './components/PwaUpdatePrompt/PwaUpdatePrompt';
+
+posthog.init(import.meta.env.VITE_POSTHOG_PROJECT_TOKEN, {
+    api_host: import.meta.env.VITE_POSTHOG_HOST,
+    capture_pageview: 'history_change',
+    capture_pageleave: true,
+    disable_session_recording: true,
+    loaded: (ph) => {
+        if (import.meta.env.DEV) ph.opt_out_capturing();
+    },
+});
 
 const store = new AppState();
 
