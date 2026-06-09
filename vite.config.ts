de927e63e9aa79db824582d577adba31f45e7504
Khaled <121107036+ivxsm@users.noreply.github.com>
2026-06-09 19:12:21 +0300

    renamed reop

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ type CreditsLicense = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/ntsc-rs-web-text/',
     plugins: [
         preact(),
         wasm() as Plugin,
